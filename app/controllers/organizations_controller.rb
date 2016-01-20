cde24eed414cdd28a8a6de87f2b48ac00de331e3
Nithyanandan KM <nithyanandankm@gmail.com>
2016-01-20 21:49:47 +0530

    Added a page for accessing the API access token CSS fixes

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -63,6 +63,10 @@ class OrganizationsController < ApplicationController
     end
   end
 
+  def api_keys
+    @api_key = current_user.api_creds
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_organization
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :model_types
   resources :models
-  resources :organizations
+  resources :organizations do
+    collection do
+      get 'api_keys'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
